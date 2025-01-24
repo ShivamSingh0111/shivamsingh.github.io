e860eb126431d4ff1616b4518d5bdf4629ae6917
ShivamSingh0111 <rajputshivamsinghrkt98@gmail.com>
2025-01-24 11:26:10 +0530

    all task are completed

---
+++ scriptAndPages/projects.js
@@ -0,0 +1,88 @@
+let projectsSection = document.getElementById("mainDivProjectsSection");
+projectsSection.innerHTML = `
+
+            <!-- Discovery project -->
+            <div style="border-top: none;" class="eachProjectDiv EVehicle">
+                <p class="ProjectName ">CaloriScan</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/discovery.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                       
+                       
+                        <br />
+                        <p>
+                        CaloriesScanner is an innovative web application that scans QR codes of food 
+                        dishes and analyzes their calorie, nutrient, and mineral content. By integrating 
+                        powerful technologies such as Google Vision, Object Detection, and OpenAI API, 
+                        CaloriesScanner provides users with detailed insights into their food choices.
+                        </p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-react"></i> React</p>
+                            <p><svg width="15" height="15" viewBox="0 0 256 208" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet"><path d="M15.624 35.012v87.53l15.058-8.471V43.482l-15.058-8.47z" fill="white"/><path d="M.565 114.07V8.66l89.411 50.823v17.883L15.624 35.012v87.53L.564 114.07z" fill="white"/><path d="M181.27 8.659V114.07l-60.235 33.882-15.059-9.412 60.236-33.882V34.07L89.976 77.365V59.482L181.271 8.66z" fill="white"/><path d="M166.212.188L90.918 42.541 15.624.188.564 8.658l89.412 50.824L181.271 8.66 166.21.189z" fill="white"/><path d="M151.153 42.541v53.647l15.059 8.47V34.072l-15.06 8.47z" fill="white"/><path d="M75.859 138.541l75.294-42.353 15.059 8.47-60.236 33.883 59.295 33.883 60.235-33.883 15.059 8.47-75.294 42.354-89.412-50.824z" fill="white"/><path d="M75.859 138.541v17.883l89.412 50.823v-17.882L75.859 138.54zM240.565 147.012V94.306l-15.06-8.47v52.705l15.06 8.47z" fill="white"/><path d="M165.27 189.365l75.295-42.353V94.306l15.059-8.47v70.588l-90.353 50.823v-17.882z" fill="white"/><path d="M255.624 85.835l-15.06 8.47-15.058-8.47 15.059-8.47 15.059 8.47z" fill="white"/><path d="M240.565 77.365V59.482l15.059-8.47v17.882l-15.06 8.47z" fill="white"/><path d="M240.565 59.482v17.883l-15.06-8.47V51.011l15.06 8.47z" fill="white"/><path d="M255.624 51.012l-15.06 8.47-15.058-8.47 15.059-8.47 15.059 8.47z" fill="white"/></svg> Material UI</p>
+                            <p> <i class="fab fa-node-js"></i> NodeJs</p>
+                           
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/TheApostle-07/CaloriScan" target="blank"><button>
+                                    Github</button></a>
+                            <a href="https://calori-scan-frontend.vercel.app/" target="blank"><button>Demo</button></a>
+                            
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- lybrate project -->
+            
+            
+            <!-- Tic Tac Toe project -->
+           
+            
+            <!-- Recipe Searching project -->
+            <div class="eachProjectDiv">
+                <p class="ProjectName"><strong>ShopKart</strong> is a E-Commerce shopping website</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/ShopKart.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                        <p>Shop Cart is a clone of the popular e-commerce website Flipkart. 
+                        The project features a variety of products for sale, including electronics, groceries, 
+                        and clothing for men and women.
+                         It also implements localStorage for user login and registration functionality. </p>
+                        <p>This peoject is designed using HTML, CSS and JavaScript</p>
+                        <br />
+                        <p> 
+                        </p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+
+                        <br>
+                        <div class="techStackProject">
+                            <p> <i class="fab fa-js-square"></i> JavaScript </p>
+                            
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                          
+                        </div>
+                         <div class="ProjectLinks">
+                            <a href="https://github.com/ShivamSingh0111/B41_WEB015_The-Coder-Collective" target="blank"><button>
+                                    Github</button></a>
+                            <a href="https://shopcart.netlify.app/" target="blank"><button>Demo</button></a>
+                           
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+
+
+`;
